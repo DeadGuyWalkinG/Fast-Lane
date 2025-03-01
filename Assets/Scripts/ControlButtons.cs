35f152d798727e6eb35ca2d0b2306bddea16de48
Antony Sajan <antonysajant@gmail.com>
2025-03-01 23:51:01 +0530

    Configuring controls

---
+++ Assets/Scripts/ControlButtons.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+
+public class ControlButtons : MonoBehaviour
+{
+    [SerializeField] private GameObject car;
+    [SerializeField] private float speed = 10f;
+    
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("Move Left")]
+    public void leftButton()
+    {
+
+        StartCoroutine(changeLeft());
+    }
+
+    public void brake()
+    {
+    }
+
+    [ContextMenu("Move Right")]
+    public void rightButton()
+    {
+        StartCoroutine(changeRight());
+    }
+
+    IEnumerator changeLeft()
+    { 
+        while(car.transform.position.x >= -0.91f)
+        {
+            car.transform.Translate(Vector2.left * speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    IEnumerator changeRight()
+    {
+        while (car.transform.position.x <= 0.91f)
+        {
+            car.transform.Translate(Vector2.right * speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
